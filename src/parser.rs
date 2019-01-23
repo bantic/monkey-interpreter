cd27fa6a62075877ee1b9642e1d82fbbc8758bd8
Cory <cory.forsyth@gmail.com>
2019-01-23 12:51:01 -0500

    WIP: start parser. does not compile

---
+++ src/parser.rs
@@ -0,0 +1,20 @@
+// use super::lexer;
+// use super::token;
+
+// #[derive(Debug)]
+// pub struct Parser<'a> {
+//   lex: &'a lexer::Lexer<'a>,
+// }
+
+// trait Node<'a> {
+//   fn literal() -> token::Token<'a>;
+// }
+
+// trait Statement<'a> {
+//   fn literal() -> token::Token<'a>;
+//   fn expression();
+// }
+
+// struct Program<e: Statement<'a>> {
+//   statements: 'a Vec<T>,
+// }
