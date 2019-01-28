9ab3a11d492e13431608f45d075f7c6770622bec
Cory <cory.forsyth@gmail.com>
2019-01-28 17:00:06 -0500

    Initial Parser and AST work

--- src/parser.rs
+++ src/parser.rs
@@ -1,10 +1,62 @@
-// use super::lexer;
-// use super::token;
+use super::lexer;
+use super::token;
+
+// Node
+//
+// Program
+//  - statements: Vec<StatementNode>
+//
+//  ExpressionNode
+//
+// pub struct program {
+//
+// }
+
+pub enum Operator {
+  Minus,
+  Plus,
+}
+
+pub enum Expression {
+  Literal(i32),
+  Binary(Box<Expression>, Operator, Box<Expression>),
+  Unary(Operator, Box<Expression>),
+}
+
+pub enum Statement {
+  Let(String, Expression),
+}
+
+//pub struct program {
+//  stmts: Vec<syntax>,
+//}
+
+pub struct Program(Vec<Statement>);
+// pub struct stmt(Vec<syntax>);
 
 // #[derive(Debug)]
-// pub struct Parser<'a> {
-//   lex: &'a lexer::Lexer<'a>,
-// }
+pub struct Parser<'a> {
+  lex: &'a lexer::Lexer<'a>,
+  cur: token::Token<'a>,
+  peek: token::Token<'a>,
+}
+
+impl<'a> Parser<'a> {
+  pub fn new(lex: &'a lexer::Lexer) -> Parser<'a> {
+    let cur = lex.next_token();
+    let peek = lex.next_token();
+    Parser {
+      lex,
+      cur: cur,
+      peek: peek,
+    }
+  }
+
+  pub fn next_token(&mut self) {
+    self.cur = self.peek.clone();
+    self.peek = self.lex.next_token();
+  }
+}
 
 // trait Node<'a> {
 //   fn literal() -> token::Token<'a>;
@@ -18,3 +70,19 @@
 // struct Program<e: Statement<'a>> {
 //   statements: 'a Vec<T>,
 // }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_it() {
+    assert_eq!(
+      parse("let x = 5;"),
+      Program(vec![Statement::Let(
+        "x".to_string(),
+        Expression::Literal(5)
+      ),])
+    );
+  }
+}
--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Token<'a> {
   Illegal,
   Eof,
