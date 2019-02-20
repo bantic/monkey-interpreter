5b22f006b3d74cc20bcf6fbad29d4547163fa9a9
Cory <cory.forsyth@gmail.com>
2019-02-20 12:03:54 -0500

    make repl nicer

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use std::io;
 fn main() {
   // get stdio
   let repl = io::stdin();
+  println!("Welcome to Monkey! Write code.");
   loop {
     let mut line = String::new();
     repl.read_line(&mut line).expect("Failed to read line");
     let lex = lexer::Lexer::new(&line);
+    let parser = parser::Parser::new(&lex);
+    println!("AST: {:?}", parser.parse());
+    let lex = lexer::Lexer::new(&line);
+    println!("Tokens:");
     loop {
       let tok = lex.next_token();
       println!("{:?}", tok);
